f501f7f36e7009867908a716f32388ab4b23d5f1
chen-justin <jnzheng8@gmail.com>
2025-12-06 20:48:11 -0500

    bugfix(): favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <link rel="icon" href="/icon.png" />
       <body className={font.className}>{children}</body>
     </html>
   );
